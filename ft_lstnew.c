f9c30432292ec5e856b73db849dbf9a15a2804bc
Alexander Carlson <acarlson@e1z3r3p10.42.us.org>
2018-10-04 12:08:22 -0700

    ft_lstnew now sets new->next to NULL

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 18:06:04 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/22 19:52:17 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/04 11:18:52 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 		ft_memcpy(new->content, content, content_size);
 		new->content_size = content_size;
 	}
+	new->next = NULL;
 	return (new);
 }
