1f1ada0a226e1a2ea4857af019ccbca3cac0bdee
Alexander Carlson <acarlson@e1z2r5p13.42.us.org>
2018-10-06 18:01:33 -0700

    Fixed most norm errors.  Still 29 lines.  Still might not work idk

--- ft_lstsort.c
+++ ft_lstsort.c
@@ -6,49 +6,49 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/02 10:41:57 by acarlson          #+#    #+#             */
-/*   Updated: 2018/10/06 17:49:07 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/06 17:59:09 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void thing(void *content, size_t size)
+static void		thing(void *content, size_t size)
 {
-  if (size == 0)
-    return;
-  else
-    free(content);
+	if (size == 0)
+		return ;
+	else
+		free(content);
 }
 
-t_list          *ft_lstsort(t_list **lst, int (*cmp)(t_list *a, t_list *b))
+t_list			*ft_lstsort(t_list **lst, int (*cmp)(t_list *a, t_list *b))
 {
-    t_list  *head;
-    t_list  *tmp;
-    t_list  *t2;
-    int     done;
+	t_list	*head;
+	t_list	*tmp;
+	t_list	*t2;
+	int		done;
 
-    ft_lstadd(&*lst, ft_lstnew(NULL, sizeof(NULL)));
-    head = *lst;
-    done = 0;
-    while (!(done))
-    {
-        done = 1;
-        while ((*lst)->next->next)
-        {
-            if (cmp((*lst)->next, (*lst)->next->next) > 0)
-            {
-                done = 0;
-                tmp = (*lst)->next->next;
-                t2 = (*lst)->next;
-                t2->next = (*lst)->next->next->next;
-                tmp->next = t2;
-                (*lst)->next = tmp;
-            }
-            *lst = (*lst)->next;
-        }
-        *lst = head;
-    }
-    *lst = head->next;
-    ft_lstdelone(&head, thing);
-    return (*lst);
+	ft_lstadd(&*lst, ft_lstnew(NULL, sizeof(NULL)));
+	head = *lst;
+	done = 0;
+	while (!(done))
+	{
+		done = 1;
+		while ((*lst)->next->next)
+		{
+			if (cmp((*lst)->next, (*lst)->next->next) > 0)
+			{
+				done = 0;
+				tmp = (*lst)->next->next;
+				t2 = (*lst)->next;
+				t2->next = (*lst)->next->next->next;
+				tmp->next = t2;
+				(*lst)->next = tmp;
+			}
+			*lst = (*lst)->next;
+		}
+		*lst = head;
+	}
+	*lst = head->next;
+	ft_lstdelone(&head, thing);
+	return (*lst);
 }
