25feb0bceae7e6124cce080a5c71b5cf0d0751df
Alexander Carlson <acarlson@e1z2r5p13.42.us.org>
2018-10-06 17:51:21 -0700

    Added lstsort.  Terrible way of sorting, but it mostly works I think

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 16:13:51 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/30 16:23:33 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/06 17:50:00 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,7 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list			*ft_lstsort(t_list **lst, int (*cmp)(t_list *a, t_list *b));
 
 int				ft_isprime(unsigned long n);
 char			*ft_strndup(const char *s1, size_t n);
