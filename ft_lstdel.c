b047bc55c479bd7a15d86677dbf529b9fd00f832
Alexander Carlson <acarlson@e1z2r5p13.42.us.org>
2018-10-06 21:59:27 -0700

    lstdel now calls lstdelone on each element of the list

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 20:20:17 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/23 14:35:44 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/06 21:58:19 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void		ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	t_list		*next;
+
 	while (*alst)
 	{
-		del((*alst)->content, (*alst)->content_size);
-		free(*alst);
-		*alst = (*alst)->next;
+		next = (*alst)->next;
+		ft_lstdelone(alst, del);
+		*alst = next;
 	}
-	*alst = NULL;
 }
