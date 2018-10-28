c477af1815a5b72e5bed524624031919c726735d
Alexander Carlson <acarlson@e1z3r5p23.42.us.org>
2018-10-28 12:32:44 -0700

    Strjoin now allocates enough space for '\0'.  No idea how moulinette and 42FileChecker ignore this

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 13:58:25 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/17 22:36:15 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/28 12:29:22 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,14 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	size_t	len1;
 	size_t	len2;
 	char	*new;
-	char	*ptr;
 
 	if (s1 && s2)
 	{
 		len1 = ft_strlen(s1);
 		len2 = ft_strlen(s2);
-		NULL_CHECK(!(new = (char *)malloc((len1 + len2) * sizeof(char))));
-		ptr = new;
-		while (*s1)
-			*ptr++ = *s1++;
-		while (*s2)
-			*ptr++ = *s2++;
-		*ptr = '\0';
+		NULL_CHECK(!(new = (char *)malloc((len1 + len2 + 1) * sizeof(new))));
+		ft_strcpy(new, s1);
+		ft_strcat(new, s2);
 		return (new);
 	}
 	return (NULL);
