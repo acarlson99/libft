403003969b35ae88f1e6607cc8e75c9613b919c3
Alexander Carlson <acarlson@e1z3r5p5.42.us.org>
2018-10-29 21:06:56 -0700

    Made memset faster

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 16:41:16 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/12 14:40:01 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/10/29 21:06:29 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char *ptr;
-
-	ptr = (unsigned char*)b;
-	while (len > 0)
-	{
-		*ptr = (unsigned char)c;
-		ptr++;
-		len--;
-	}
+	while (len--)
+		*((unsigned char *)b + len) = (unsigned char)c;
 	return (b);
 }
