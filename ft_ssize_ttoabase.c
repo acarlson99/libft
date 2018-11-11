6c7d93077e46fa95c8b8bb5f171d81c7b9c83a8a
Alexander Carlson <acarlson@e1z3r1p5.42.us.org>
2018-11-11 14:59:43 -0800

    Fixed norm error

--- ft_ssize_ttoabase.c
+++ ft_ssize_ttoabase.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:59:02 by acarlson          #+#    #+#             */
-/*   Updated: 2018/11/09 13:14:36 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/11/11 14:59:24 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char		*ft_ssize_ttoabase(ssize_t n, int base)
 {
-	char	*new;
+	char		*new;
 	ssize_t		is_neg;
 	ssize_t		i;
 
