630a39ed73ddf8f8d193ddbded3dddd6ec7763cc
Alexander Carlson <acarlson@e1z3r1p21.42.us.org>
2018-11-12 17:05:25 -0800

    Fixed unumlen error returning 0 if n == 0

--- ft_unumlen.c
+++ ft_unumlen.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 13:33:11 by acarlson          #+#    #+#             */
-/*   Updated: 2018/11/09 13:33:51 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/11/12 17:02:43 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t		ft_unumlen(size_t n, int base)
 	size_t		count;
 
 	count = 0;
-	RET_IF(n == 0, 0);
+	RET_IF(n == 0, 1);
 	while (n != 0)
 	{
 		count++;
