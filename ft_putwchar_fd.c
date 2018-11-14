cee3f8e03dac435099c963d6cec1bf151f024223
Alexander Carlson <acarlson@e1z3r1p23.42.us.org>
2018-11-13 20:48:04 -0800

    wcharlen and putwchar seem to be working

--- ft_putwchar_fd.c
+++ ft_putwchar_fd.c
@@ -6,30 +6,30 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 20:11:32 by acarlson          #+#    #+#             */
-/*   Updated: 2018/11/13 20:11:44 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/11/13 20:31:26 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_putwchar_fd(wchar_t c, int fd)
+int		ft_putwchar_fd(wchar_t wc, int fd)
 {
 	int		uni_len;
 
-	uni_len = ft_unicode_len(c);
+	uni_len = ft_unicode_len(wc);
 	if (uni_len == 1)
-		return (ft_putchar_fd_2(c, fd));
+		return (ft_putchar_fd_2(wc, fd));
 	else if (uni_len == 2)
-		return (ft_putchar_fd_2((c >> 6) + 0xC0, fd)\
-						+ ft_putchar_fd_2(c & 0xBF, fd));
+		return (ft_putchar_fd_2((wc >> 6) + 0xC0, fd)\
+						+ ft_putchar_fd_2(wc & 0xBF, fd));
 	else if (uni_len == 3)
-		return (ft_putchar_fd_2((c >> 12) + 0xE0, fd)\
-						+ ft_putchar_fd_2(((c >> 6) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2((c & 0x3F) + 0x80, fd));
+		return (ft_putchar_fd_2((wc >> 12) + 0xE0, fd)\
+						+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
+						+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
 	else if (uni_len == 4)
-		return (ft_putchar_fd_2((c >> 18) + 0xF0, fd)\
-						+ ft_putchar_fd_2(((c >> 12) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2(((c >> 6) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2((c & 0x3F) + 0x80, fd));
+		return (ft_putchar_fd_2((wc >> 18) + 0xF0, fd)\
+						+ ft_putchar_fd_2(((wc >> 12) & 0x3F) + 0x80, fd)\
+						+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
+						+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
 	return (uni_len);
 }
