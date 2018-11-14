40fef49b8b39cb3c31b9fe1bc79e750384f89665
Alexander Carlson <acarlson@e1z3r1p23.42.us.org>
2018-11-13 21:42:18 -0800

    Fixed weird spacing norm error

--- ft_putwchar_fd.c
+++ ft_putwchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 20:11:32 by acarlson          #+#    #+#             */
-/*   Updated: 2018/11/13 20:49:07 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/11/13 21:39:51 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ int		ft_putwchar_fd(wchar_t wc, int fd)
 		return (ft_putchar_fd_2(wc, fd));
 	else if (uni_len == 2)
 		return (ft_putchar_fd_2((wc >> 6) + 0xC0, fd)\
-						+ ft_putchar_fd_2(wc & 0xBF, fd));
+				+ ft_putchar_fd_2(wc & 0xBF, fd));
 	else if (uni_len == 3)
 		return (ft_putchar_fd_2((wc >> 12) + 0xE0, fd)\
-						+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
+				+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
+				+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
 	else if (uni_len == 4)
+	{
 		return (ft_putchar_fd_2((wc >> 18) + 0xF0, fd)\
-						+ ft_putchar_fd_2(((wc >> 12) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
-						+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
+				+ ft_putchar_fd_2(((wc >> 12) & 0x3F) + 0x80, fd)\
+				+ ft_putchar_fd_2(((wc >> 6) & 0x3F) + 0x80, fd)\
+				+ ft_putchar_fd_2((wc & 0x3F) + 0x80, fd));
+	}
 	return (uni_len);
 }
