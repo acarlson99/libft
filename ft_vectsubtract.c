40c2bfe145e7858b751368be1bcbbe3b3196bf26
Alexander Carlson <acarlson@e1z3r6p6.42.us.org>
2019-01-07 21:39:02 -0800

    Fixed silly name error

--- ft_vectsubtract.c
+++ ft_vectsubtract.c
@@ -6,13 +6,13 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 14:05:31 by acarlson          #+#    #+#             */
-/*   Updated: 2018/12/18 10:27:23 by acarlson         ###   ########.fr       */
+/*   Updated: 2019/01/07 21:36:09 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_vect3	*ft_vectadd(t_vect3 *v, t_vect3 *w)
+t_vect3	*ft_vectsubtract(t_vect3 *v, t_vect3 *w)
 {
 	return (ft_vectnew(v->x - w->x, v->y - w->y, v->z - w->z));
 }
