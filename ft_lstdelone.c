e91b1d87ccc2bc505ddefe4bcf71b8d73729f161
Alexander <alexandercarlson99@gmail.com>
2019-07-18 14:33:36 -0700

    Fixed ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 19:53:40 by acarlson          #+#    #+#             */
-/*   Updated: 2018/09/22 20:18:53 by acarlson         ###   ########.fr       */
+/*   Updated: 2019/07/18 14:32:57 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void		ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
+	if (del)
+		del((*alst)->content, (*alst)->content_size);
 	free(*alst);
 	*alst = NULL;
 }
